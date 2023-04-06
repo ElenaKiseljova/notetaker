f887cbe638707a77bf48c760f3524c2eff8d1d28
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-04-06 18:52:09 +0300

    Added Nest Image to the content Markdown

--- src/components/note/NoteCard.tsx
+++ src/components/note/NoteCard.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import Image from 'next/image';
 
 import type { RouterOutputs } from '~/utils/api';
 
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { type Components } from 'react-markdown';
 
 type TNote = RouterOutputs['note']['getAll'][0];
 
@@ -13,6 +15,21 @@ type TNoteCardProps = {
 
 const NoteCard: React.FC<TNoteCardProps> = ({note, onDelete}) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const customRenderers: Components = {
+   img(props) {
+      const { src, alt } = props
+
+      return (
+        <Image
+          src={typeof src === 'string' && src.length > 0 ? src : '#'}
+          alt={typeof alt === 'string' && alt.length > 0 ? alt : 'Next image'}
+          width={200}
+          height={400}
+        />
+      );
+    },
+  };
 
   return (
     <div className=' card mt-5 border border-gray-200 bg-base-100 shadow-xl'>
@@ -28,7 +45,7 @@ const NoteCard: React.FC<TNoteCardProps> = ({note, onDelete}) => {
 
           <div className="collapse-content"> 
             <article className="prose lg:prose-xl">
-              <ReactMarkdown>{note.content}</ReactMarkdown>
+              <ReactMarkdown components={customRenderers}>{note.content}</ReactMarkdown>
             </article>            
           </div>
         </div>
